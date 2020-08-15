c833375f450cae95877c34ac7a94bfbc2e6adb52
Vincent Haupert <mail@vincent-haupert.de>
2020-08-15 12:45:49 +0200

    Add method to read test resources omitting comments

---
+++ src/test/kotlin/devicecheck/appattest/ResourceUtils.kt
@@ -0,0 +1,4 @@
+package devicecheck.appattest
+
+fun <T> Class<T>.readTextResource(name: String, commentLinePrefix: String = "#"): String
+    = getResource(name).readText().split("\n").filterNot { it.startsWith("#") }.joinToString("\n")
