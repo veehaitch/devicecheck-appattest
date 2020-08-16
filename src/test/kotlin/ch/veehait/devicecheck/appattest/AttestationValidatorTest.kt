e5c1d4634565cdb983ccfd7937ffee64e23dfa36
Vincent Haupert <mail@vincent-haupert.de>
2020-08-17 01:27:18 +0200

    AttestationValidator: add builtin Apple root CA

--- src/test/kotlin/ch/veehait/devicecheck/appattest/AttestationValidatorTest.kt
+++ src/test/kotlin/ch/veehait/devicecheck/appattest/AttestationValidatorTest.kt
@@ -2,34 +2,23 @@ package ch.veehait.devicecheck.appattest
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
-import java.lang.IllegalArgumentException
-import java.net.URL
 
 class AttestationValidatorTest : StringSpec() {
-
-    private val appleAppAttestRootCA = downloadAppAttestCA()
-
-    private fun downloadAppAttestCA(): String {
-        return URL("https://www.apple.com/certificateauthority/Apple_App_Attestation_Root_CA.pem")
-            .openStream().bufferedReader().readText()
-    }
-
     init {
         "team identifier must consist of 10 characters" {
             shouldThrow<IllegalArgumentException> {
-                AttestationValidator("a".repeat(9), "", appleAppAttestRootCA, AppleAppAttestEnvironment.DEVELOPMENT)
+                AttestationValidator("a".repeat(9), "", AppleAppAttestEnvironment.DEVELOPMENT)
             }
             shouldThrow<IllegalArgumentException> {
-                AttestationValidator("a".repeat(11), "", appleAppAttestRootCA, AppleAppAttestEnvironment.DEVELOPMENT)
+                AttestationValidator("a".repeat(11), "", AppleAppAttestEnvironment.DEVELOPMENT)
             }
-            AttestationValidator("a".repeat(10), "", appleAppAttestRootCA, AppleAppAttestEnvironment.DEVELOPMENT)
+            AttestationValidator("a".repeat(10), "", AppleAppAttestEnvironment.DEVELOPMENT)
         }
 
         "validation works for valid attestation object" {
             AttestationValidator(
                 "6MURL8TA57",
                 "de.vincent-haupert.AppleAppAttestPoc",
-                appleAppAttestRootCA,
                 AppleAppAttestEnvironment.DEVELOPMENT)
                 .validate(
                     javaClass.readTextResource("/iOS14-attestation-response-base64.cbor"),
