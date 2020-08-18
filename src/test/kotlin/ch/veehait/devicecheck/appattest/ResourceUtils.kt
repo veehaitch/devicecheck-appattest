9271cf3ed64227ce300033b17bd992149b83d9d2
Vincent Haupert <mail@vincent-haupert.de>
2020-08-18 09:28:50 +0200

    ResourceUtils: use parameter `commentLinePrefix`

--- src/test/kotlin/ch/veehait/devicecheck/appattest/ResourceUtils.kt
+++ src/test/kotlin/ch/veehait/devicecheck/appattest/ResourceUtils.kt
@@ -1,4 +1,4 @@
 package ch.veehait.devicecheck.appattest
 
 fun <T> Class<T>.readTextResource(name: String, commentLinePrefix: String = "#"): String
-    = getResource(name).readText().split("\n").filterNot { it.startsWith("#") }.joinToString("\n")
+    = getResource(name).readText().split("\n").filterNot { it.startsWith(commentLinePrefix) }.joinToString("\n")
