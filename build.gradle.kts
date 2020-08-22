c8b4a63a78802ae96d77e628d9a36770cb973151
Vincent Haupert <mail@vincent-haupert.de>
2020-08-22 09:03:08 +0200

    Gradle: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
-    id("io.gitlab.arturbosch.detekt") version "1.10.0"
+    id("org.jetbrains.kotlin.jvm") version "1.4.0"
+    id("io.gitlab.arturbosch.detekt") version "1.11.2"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
@@ -26,12 +26,19 @@ dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
     // Kotlin coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
 
     // CBOR
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.+")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.11.+")
+    val jacksonVersion = "2.11.2"
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:${jacksonVersion}")
 
     // Bouncy Castle
     implementation("org.bouncycastle:bcprov-jdk15on:1.66")
@@ -39,16 +46,11 @@ dependencies {
     // WebAuthN parsing
     implementation("com.webauthn4j:webauthn4j-core:0.12.0.RELEASE")
 
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-
     // Kotest
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.1.2") // for kotest framework
-    testImplementation("io.kotest:kotest-assertions-core-jvm:4.1.2") // for kotest core jvm assertions
-    testImplementation("io.kotest:kotest-property-jvm:4.1.2") // for kotest property test
+    val kotestVersion = "4.2.0"
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:${kotestVersion}") // for kotest framework
+    testImplementation("io.kotest:kotest-assertions-core-jvm:${kotestVersion}") // for kotest core jvm assertions
+    testImplementation("io.kotest:kotest-property-jvm:${kotestVersion}") // for kotest property test
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
