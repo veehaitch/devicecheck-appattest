ae3ee7fab769472868ef54507d07958b1dbd303e
Vincent Haupert <mail@vincent-haupert.de>
2020-08-23 12:39:12 +0200

    Class<T>.readTextResource: filter blank lines

--- src/test/kotlin/ch/veehait/devicecheck/appattest/ResourceUtils.kt
+++ src/test/kotlin/ch/veehait/devicecheck/appattest/ResourceUtils.kt
@@ -1,4 +1,5 @@
 package ch.veehait.devicecheck.appattest
 
 fun <T> Class<T>.readTextResource(name: String, commentLinePrefix: String = "#"): String =
-    getResource(name).readText().split("\n").filterNot { it.startsWith(commentLinePrefix) }.joinToString("\n")
+    getResource(name).readText().split("\n")
+        .filterNot { it.startsWith(commentLinePrefix) || it.isBlank() }.joinToString("\n")
