e8a848c3a1b9b85a2800c4b28caae6ecf60f88ee
Vincent Haupert <mail@vincent-haupert.de>
2020-08-31 18:09:16 +0200

    ReceiptExchange: Add test

---
+++ src/test/kotlin/ch/veehait/devicecheck/appattest/receipt/ReceiptExchangeTest.kt
@@ -0,0 +1,68 @@
+package ch.veehait.devicecheck.appattest.receipt
+
+import ch.veehait.devicecheck.appattest.Extensions.toBase64
+import ch.veehait.devicecheck.appattest.attestation.AppleAppAttestEnvironment
+import ch.veehait.devicecheck.appattest.attestation.AttestationSample
+import ch.veehait.devicecheck.appattest.attestation.AttestationValidator
+import ch.veehait.devicecheck.appattest.readTextResource
+import com.fasterxml.jackson.core.JsonFactory
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.kotest.core.spec.style.StringSpec
+import java.time.Clock
+import java.time.ZoneOffset
+
+class ReceiptExchangeTest : StringSpec() {
+
+    private val jsonObjectMapper = ObjectMapper(JsonFactory())
+        .registerModule(JavaTimeModule())
+        .registerModule(KotlinModule())
+
+    init {
+
+        val appleDeviceCheckKid = "94M3Z58NQ7"
+        val appleDeviceCheckPrivateKeyPem = System.getenv("APPLE_KEY_P8_$appleDeviceCheckKid")
+
+        "receipt exchange works".config(enabled = appleDeviceCheckPrivateKeyPem != null) {
+            // Test setup
+            val attestationSampleJson = javaClass.readTextResource("/iOS14-attestation-sample.json")
+            val attestationSample: AttestationSample = jsonObjectMapper.readValue(attestationSampleJson)
+
+            val attestationSampleCreationTimeClock = Clock.fixed(
+                attestationSample.timestamp.plusSeconds(5),
+                ZoneOffset.UTC
+            )
+
+            val attestationResponse = AttestationValidator(
+                appTeamIdentifier = attestationSample.teamIdentifier,
+                appBundleIdentifier = attestationSample.bundleIdentifier,
+                appleAppAttestEnvironment = AppleAppAttestEnvironment.DEVELOPMENT,
+                clock = attestationSampleCreationTimeClock
+            ).validate(
+                attestationObject = attestationSample.attestation,
+                keyIdBase64 = attestationSample.keyId.toBase64(),
+                serverChallenge = attestationSample.clientData
+            )
+
+            // Actual test
+            val receiptExchange = ReceiptExchangeImpl(
+                appleJwsGenerator = AppleJwsGeneratorImpl(
+                    appleTeamIdentifier = attestationSample.teamIdentifier,
+                    keyIdentifier = appleDeviceCheckKid,
+                    privateKeyPem = appleDeviceCheckPrivateKeyPem
+                ),
+                receiptValidator = ReceiptValidator(
+                    appTeamIdentifier = attestationSample.teamIdentifier,
+                    appBundleIdentifier = attestationSample.bundleIdentifier,
+                ),
+            )
+
+            val newReceipt = receiptExchange.trade(
+                receiptP7 = attestationResponse.receipt.p7,
+                attestationPublicKey = attestationResponse.publicKey
+            )
+        }
+    }
+}
