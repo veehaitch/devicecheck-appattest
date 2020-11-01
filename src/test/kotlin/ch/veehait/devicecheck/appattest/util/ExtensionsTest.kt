7707457cf0473c39d440d133bb3650892f7631d1
Vincent Haupert <mail@vincent-haupert.de>
2020-11-01 18:17:26 +0100

    Add test for UUID (AAGUID) parsing

--- src/test/kotlin/ch/veehait/devicecheck/appattest/util/ExtensionsTest.kt
+++ src/test/kotlin/ch/veehait/devicecheck/appattest/util/ExtensionsTest.kt
@@ -2,9 +2,11 @@ package ch.veehait.devicecheck.appattest.util
 
 import ch.veehait.devicecheck.appattest.util.Extensions.readAsUInt16
 import ch.veehait.devicecheck.appattest.util.Extensions.readAsUInt32
+import ch.veehait.devicecheck.appattest.util.Extensions.toUUID
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.comparables.shouldBeEqualComparingTo
 import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.longs.shouldBeExactly
 import io.kotest.matchers.throwable.shouldHaveMessage
@@ -12,10 +14,12 @@ import io.kotest.property.Arb
 import io.kotest.property.Exhaustive
 import io.kotest.property.arbitrary.int
 import io.kotest.property.arbitrary.long
+import io.kotest.property.arbitrary.string
 import io.kotest.property.checkAll
 import io.kotest.property.exhaustive.ints
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+import java.util.UUID
 
 class ExtensionsTest : StringSpec() {
     init {
@@ -66,6 +70,31 @@ class ExtensionsTest : StringSpec() {
                     .takeLast(4)
                     .toByteArray()
                 arr.readAsUInt32() shouldBeExactly value
+            }
+        }
+
+        // ByteArray.toUUID()
+        "Parsing bytes as UUID requires array of maximum length 16" {
+            checkAll(Exhaustive.ints(0 until 32)) { size ->
+                when {
+                    size <= 16 -> shouldNotThrow<IllegalArgumentException> { ByteArray(size).toUUID() }
+                    else -> shouldThrow<IllegalArgumentException> {
+                        ByteArray(size).toUUID()
+                    } shouldHaveMessage "Byte array must not contain more than 16 bytes"
+                }
+            }
+        }
+
+        "Parsing bytes as UUID works" {
+            checkAll(Arb.string()) { value ->
+                val uuid = UUID.nameUUIDFromBytes(value.toByteArray())
+                val arr = uuid.let {
+                    ByteBuffer.allocate(16)
+                        .putLong(it.mostSignificantBits)
+                        .putLong(it.leastSignificantBits)
+                }.order(ByteOrder.BIG_ENDIAN).array()
+
+                arr.toUUID() shouldBeEqualComparingTo uuid
             }
         }
     }
