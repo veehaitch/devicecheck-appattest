31f00d76b7f135aed8c35031f54338e5d0162a5e
Vincent Haupert <mail@vincent-haupert.de>
2021-09-28 14:34:25 +0200

    Fix typo

--- src/main/kotlin/ch/veehait/devicecheck/appattest/assertion/AssertionException.kt
+++ src/main/kotlin/ch/veehait/devicecheck/appattest/assertion/AssertionException.kt
@@ -2,6 +2,6 @@ package ch.veehait.devicecheck.appattest.assertion
 
 sealed class AssertionException(message: String, cause: Throwable?) : RuntimeException(message, cause) {
     class InvalidAuthenticatorData(message: String) : AssertionException(message, null)
-    class InvalidSignature(cause: Throwable? = null) : AssertionException("The assertions signature is invalid", cause)
+    class InvalidSignature(cause: Throwable? = null) : AssertionException("The assertion's signature is invalid", cause)
     class InvalidChallenge(message: String, cause: Throwable? = null) : AssertionException(message, cause)
 }
